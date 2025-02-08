8dc30f50cc3750bffaf7619bd48e313eb5651f0a
Shivam-Lad-18 <shivamvlad3@gmail.com>
2025-02-09 00:17:15 +0530

    add switch button & fix bugs

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var input_area_g = document.getElementById("Input_area");
 var sub_area_g = document.getElementById("sub_input");
 var keys = document.getElementById("keys");
+var chanbool = false;
 keys.addEventListener('click', buttonpress);
 
-const operators = new Set(["+","-","/","*","%","**"]);
+const operators = new Set(["+", "-", "/", "*", "%", "**"]);
 function setActive(clickedButton) {
     // Remove 'active' class from all buttons
     document.querySelectorAll(".nav-link").forEach(button => button.classList.remove("active"));
-
     clickedButton.classList.add("active");
 }
 function toggleActiveClass(checkbox) {
@@ -65,119 +65,251 @@ function ShowError() {
 
 window.onload = function () {
     document.getElementById("Input_area").focus();
+    var square_inv = document.getElementById("square_inv");
+    var square_root_inv = document.getElementById("square_root_inv");
+    var pow_inv = document.getElementById("**_inv");
+    var tenpow_inv = document.getElementById("tenpow_inv");
+    var log_inv = document.getElementById("log_inv");
+    var ln_inv = document.getElementById("ln_inv");
+
+    square_inv.style.display = "none";
+    square_root_inv.style.display = "none";
+    pow_inv.style.display = "none";
+    tenpow_inv.style.display = "none";
+    log_inv.style.display = "none";
+    ln_inv.style.display = "none";
 }
 function factorial(n) {
     if (n === 0 || n === 1) return 1;
     return n * factorial(n - 1);
 }
+
+function changeBtn() {
+    var square = document.getElementById("square");
+    var square_root = document.getElementById("square_root");
+    var pow = document.getElementById("**");
+    var tenpow = document.getElementById("tenpow");
+    var log = document.getElementById("log");
+    var ln = document.getElementById("ln");
+
+    var square_inv = document.getElementById("square_inv");
+    var square_root_inv = document.getElementById("square_root_inv");
+    var pow_inv = document.getElementById("**_inv");
+    var tenpow_inv = document.getElementById("tenpow_inv");
+    var log_inv = document.getElementById("log_inv");
+    var ln_inv = document.getElementById("ln_inv");
+
+    var chanbtn = document.getElementById("chan");
+    if (!chanbool) {
+        square.style.display = "none";
+        square_root.style.display = "none";
+        pow.style.display = "none";
+        tenpow.style.display = "none";
+        log.style.display = "none";
+        ln.style.display = "none";
+
+        square_inv.style.display = "block";
+        square_root_inv.style.display = "block";
+        pow_inv.style.display = "block";
+        tenpow_inv.style.display = "block";
+        log_inv.style.display = "block";
+        ln_inv.style.display = "block";
+
+        chanbool = true;
+        chanbtn.classList.add('btn-primary');
+        chanbtn.classList.remove('btn-light');
+
+
+    } else {
+        square_inv.style.display = "none";
+        square_root_inv.style.display = "none";
+        pow_inv.style.display = "none";
+        tenpow_inv.style.display = "none";
+        log_inv.style.display = "none";
+        ln_inv.style.display = "none";
+
+        square.style.display = "block";
+        square_root.style.display = "block";
+        pow.style.display = "block";
+        tenpow.style.display = "block";
+        log.style.display = "block";
+        ln.style.display = "block";
+
+        chanbool = false;
+        chanbtn.classList.add('btn-light');
+        chanbtn.classList.remove('btn-primary');
+
+    }
+}
 function buttonpress(e) {
     let val = e.target.id.trim(); // Get clicked element's ID
-
     // console.log(typeof val, val);
-
     if (val === "b_space" || val === "b_space_cont") {
         // console.log("Backspace clicked");
         if (input_area_g.value.length > 0) {
             input_area_g.value = input_area_g.value.slice(0, -1); // Remove last character
         }
     }
+    else if (e.target.closest("#chan")) {
+        changeBtn();
+    }
     else if (val === "change_sign") {
         input_area_g.value = Number(input_area_g.value) * -1;
     }
-    else if(operators.has(val)){
-        console.log("here");
-        if(input_area_g.value === "" && sub_area_g.value === ""){
-            sub_area_g.value="0"+val;
+    else if (operators.has(val)) {
+        // console.log("here");
+        if (input_area_g.value === "" && sub_area_g.value === "") {
+            sub_area_g.value = "0" + val;
             return;
         }
-        sub_area_g.value+=input_area_g.value+val;
+        else if(input_area_g.value === ""){
+            sub_area_g.value=sub_area_g.value.slice(0,sub_area_g.value.length-1)+val;
+            return;
+        }
+        if(Number(input_area_g.value)<0){
+            var opp = sub_area_g.value.at(sub_area_g.value.length-1);
+            // console.log(opp);
+            
+            if(opp == "+"){
+            sub_area_g.value = sub_area_g.value.slice(0,sub_area_g.value.length-1) +
+                                '-' + Math.abs(input_area_g.value);
+            }
+            else if(opp=='-'){
+                sub_area_g.value = sub_area_g.value.slice(0,sub_area_g.value.length-1) +
+                '+' + Math.abs(input_area_g.value);
+            }else{
+                sub_area_g.value += input_area_g.value + val;
+            }
+        }else{
+            sub_area_g.value += input_area_g.value + val;
+        }
         // input_area_g.value='';
     }
-    else if(val === 'pi'){
+    else if (val === 'pi') {
         input_area_g.value = Math.PI;
     }
-    else if(val === 'e'){
+    else if (val === 'e') {
         input_area_g.value = Math.E;
     }
     else if (val === "clear") {
-        input_area_g.value = '';
-    }
-    else if(val === "fact"){
-        if(input_area_g.value===''){
-            input_area_g.value=1;
+        if(input_area_g.value!=''){
+            input_area_g.value = '';
         }else{
+            sub_area_g.value = '';
+        }
+    }
+    else if (val === "fact") {
+        if (input_area_g.value === '') {
+            input_area_g.value = 1;
+        } else {
             console.log(Number(input_area_g.value));
             input_area_g.value = factorial(Number(input_area_g.value));
         }
     }
-    else if (val === "="){
-        var exp = sub_area_g.value + input_area_g.value;
-        if(input_area_g.value === ''){
-            input_area_g.value = eval(exp.substr(0,exp.length-1));
-            sub_area_g.value='';
+    else if (val === "=") {
+        if(Number(input_area_g.value)<0){
+            var opp = sub_area_g.value.at(sub_area_g.value.length-1);
+            // console.log(opp);
+            if(opp=="+"){
+                var exp = sub_area_g.value.slice(0,sub_area_g.value.length-1) +
+                '-' + Math.abs(input_area_g.value);
+            }else if(opp=='-'){
+                var exp = sub_area_g.value.slice(0,sub_area_g.value.length-1) +
+                '+' + Math.abs(input_area_g.value);
+            }
+        }else{
+            var exp = sub_area_g.value + input_area_g.value;
         }
-        else{
+        
+        if (input_area_g.value === '') {
+            input_area_g.value = eval(exp.substr(0, exp.length - 1));
+            sub_area_g.value = '';
+        }
+        else {
             input_area_g.value = eval(exp);
-            sub_area_g.value='';
+            sub_area_g.value = '';
         }
     }
-    else if(e.target.closest("#abs")){
+    else if (e.target.closest("#abs")) {
         // console.log("fj");
-        if(input_area_g.value<0){
+        if (input_area_g.value < 0) {
             input_area_g.value *= -1;
         }
     }
-    else if(e.target.closest("#inverse")){
-        if(input_area_g.value===''){
+    else if (e.target.closest("#inverse")) {
+        if (input_area_g.value === '') {
             console.log("fff");
             ShowError();
-        }else{
-            input_area_g.value= Math.pow(Number(input_area_g.value),-1);
+        } else {
+            input_area_g.value = Math.pow(Number(input_area_g.value), -1);
         }
     }
-    else if (e.target.closest("#square")){
-        if(input_area_g.value !=''){
-            input_area_g.value  = Math.pow(Number(input_area_g.value),2);
+    else if (e.target.closest("#square")) {
+        if (input_area_g.value != '') {
+            input_area_g.value = Math.pow(Number(input_area_g.value), 2);
         }
     }
-    else if (e.target.closest("#square_root")){
-        if(input_area_g.value !=''){
-            input_area_g.value  = Math.sqrt(Number(input_area_g.value));
+    else if (e.target.closest("#square_inv")) {
+        if (input_area_g.value != '') {
+            input_area_g.value = Math.pow(Number(input_area_g.value), 3);
         }
     }
-    else if(e.target.closest("#tenpow")){
-        if(input_area_g.value !=''){
-            input_area_g.value  = Math.pow(10,Number(input_area_g.value));
-        }else{
-            input_area_g.value=1;
+    else if (e.target.closest("#square_root")) {
+        if (input_area_g.value != '') {
+            input_area_g.value = Math.sqrt(Number(input_area_g.value));
         }
     }
-    else if(e.target.closest("#log")){
-        if(input_area_g.value !=''){
-            input_area_g.value  = Math.log10(Number(input_area_g.value));
-        }else{
+    else if (e.target.closest("#square_root_inv")) {
+        if (input_area_g.value != '') {
+            input_area_g.value = Math.cbrt(Number(input_area_g.value));
+        }
+    }
+    else if (e.target.closest("#tenpow")) {
+        if (input_area_g.value != '') {
+            input_area_g.value = Math.pow(10, Number(input_area_g.value));
+        } else {
+            input_area_g.value = 1;
+        }
+    }
+    else if (e.target.closest("#tenpow_inv")) {
+        if (input_area_g.value != '') {
+            input_area_g.value = Math.pow(2, Number(input_area_g.value));
+        } else {
+            input_area_g.value = 1;
+        }
+    }
+    else if (e.target.closest("#log")) {
+        if (input_area_g.value != '') {
+            input_area_g.value = Math.log10(Number(input_area_g.value));
+        } else {
             ShowError();
         }
     }
-    else if(e.target.closest("#ln")){
-        if(input_area_g.value !=''){
-            input_area_g.value  = Math.log(Number(input_area_g.value));
-        }else{
+    else if (e.target.closest("#ln")) {
+        if (input_area_g.value != '') {
+            input_area_g.value = Math.log(Number(input_area_g.value));
+        } else {
             ShowError();
         }
     }
-    else if (val === "."){
-        if(input_area_g.value.at(input_area_g.value.length-1)=='.'){
+    else if (e.target.closest("#ln_inv")) {
+        if (input_area_g.value != '') {
+            input_area_g.value = Math.exp(Number(input_area_g.value));
+        } else {
+            ShowError();
+        }
+    }
+    else if (val === ".") {
+        if (input_area_g.value.at(input_area_g.value.length - 1) == '.') {
             return;
         }
-        if(input_area_g.value===''){
-        input_area_g.value =0.;
+        if (input_area_g.value === '') {
+            input_area_g.value = 0.;
         }
-        input_area_g.value +=val;
+        input_area_g.value += val;
     }
     else if (!isNaN(val) && val !== "") { // Check if val is a valid number
         val = Number(val);
-
         // Prevent leading zero when pressing 0 first
         if (input_area_g.value === "0") {
             input_area_g.value = "";
